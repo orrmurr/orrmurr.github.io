3a2e8b8842c9098f7c16cf8781563f769882280d
Go Tae Gyu <>
2018-03-07 20:17:16 +0900

    no message

--- scripts/headerButtonsFunction.js
+++ scripts/headerButtonsFunction.js
@@ -1,9 +1,11 @@
 // 해당 div 다른 html 파일로 바꾸기
+var toggleSearch = false;
 var headerButtonsFunction = function(getThis){
 	if (getThis.id == "ttegululuIcon") {
 		$("#gallery").load('../templates/ttegululu.html');
 		changeGalleryPageEffect(getThis.id);
-	} else {
+	}
+	if (getThis.id == "galleryIcon") {
 		$("#gallery").load('../templates/gallery.html');
 		changeGalleryPageEffect(getThis.id);
 		repeatGalleryByJson();
@@ -11,6 +13,16 @@ var headerButtonsFunction = function(getThis){
 			$('#NewWaterfall').NewWaterfall();
 		}, setTimeoutDelay);
 	}
+	if (getThis.id == "searchIcon") {
+		toggleSearch = !toggleSearch;
+		if(toggleSearch){
+			document.getElementById("searchExtendIcon").style.display = "block";
+		}
+		else{
+			document.getElementById("searchExtendIcon").style.display = "none";
+		}
+	}
 };
 addEventListnerMouseenterAndClick("ttegululuIcon", headerButtonsFunction);
 addEventListnerMouseenterAndClick("galleryIcon", headerButtonsFunction);
+addEventListnerMouseenterAndClick("searchIcon", headerButtonsFunction);
