8e6b893669f4ecb5dc8b78a1f0729154591f1df5
Go Tae Gyu <>
2018-03-10 15:17:30 +0900

    no message

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,17 @@
 // initialize
 $(document).ready( function(){
-	setTimeoutDelay = 50;
+	setTimeoutDelay = 80;
 	importJs('parseJsonData');
 	importJs('addEventListnerMouseenterAndClick');
 	importJs('toTopButton');
 	importJs('search');
 	importJs('newWaterfall');
-	importJs('headerButtonsFunction');
 	importJs('createGallery');
 	importJs('changeGalleryPageEffect');
 	$("#gallery").load("../templates/gallery.html");
 	// DOM 생성, JSON 데이터 가져온 뒤 실행
 	setTimeout(function(){
+		importJs('headerButtonsFunction');
 		importJs('repeatGalleryByJson');
 		importJs('scrollPaging');
 		importJs('galleryClick');
