fb37d1d17781f95a929adcb35058125cd2331545
Go Tae Gyu <>
2018-03-12 16:03:50 +0900

    no message

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,27 @@
 // initialize
 $(document).ready( function(){
-	setTimeoutDelay = 100;
 	importJs('parseJsonData');
-	importJs('addEventListnerMouseenterAndClick');
-	importJs('newWaterfall');
 	setTimeout(function(){
-		importJs('toTopButton');
-		importJs('search');
-		importJs('createGallery');
-		importJs('changeGalleryPageEffect');
-		$("#gallery").load("../templates/gallery.html");
-		// DOM 생성, JSON 데이터 가져온 뒤 실행
+		setTimeoutDelay = getParseJsonData.length; //getParseJsonData 로딩후 적용
+		importJs('addEventListnerMouseenterAndClick');
+		importJs('newWaterfall');
 		setTimeout(function(){
-			importJs('headerButtonsFunction');
-			importJs('repeatGalleryByJson');
-			importJs('scrollPaging');
-			importJs('galleryClick');
+			importJs('toTopButton');
+			importJs('search');
+			importJs('createGallery');
+			importJs('changeGalleryPageEffect');
+			$("#gallery").load("../templates/gallery.html");
+			// DOM 생성, JSON 데이터 가져온 뒤 실행
 			setTimeout(function(){
-				$('#NewWaterfall').NewWaterfall();
-			}, setTimeoutDelay);			
+				importJs('headerButtonsFunction');
+				importJs('repeatGalleryByJson');
+				importJs('scrollPaging');
+				importJs('galleryClick');
+				setTimeout(function(){
+					$('#NewWaterfall').NewWaterfall();
+				}, setTimeoutDelay);
+			}, setTimeoutDelay);
 		}, setTimeoutDelay);
-	}, setTimeoutDelay);
+	}, 100);
+
 });
