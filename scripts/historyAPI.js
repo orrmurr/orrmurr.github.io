95abfbe91cdb084b99ba4e6580822908fccb2955
DESKTOP-OKV31AN\Go Teagyu <y2k-112@daum.net>
2018-05-10 16:13:54 +0900

    no message

--- scripts/historyAPI.js
+++ scripts/historyAPI.js
@@ -1,10 +1,10 @@
 var historyAPIFunction = function(getThis){
   if (getThis.id == "searchIcon") {
       history.pushState({ data: 'pushpush' }, 'search로', '/search')
-      history.replaceState({ data: 'replace' }, 'search로', '/search');
+      // history.replaceState({ data: 'replace' }, 'search로', '/search');
   } else {
       history.pushState({ data: getThis.id }, 'getThis.id로', '/'+getThis.id)
-      history.replaceState({ data: getThis.id }, 'getThis.id로', '/'+getThis.id);
+      // history.replaceState({ data: getThis.id }, 'getThis.id로', '/'+getThis.id);
   }
 };
 
