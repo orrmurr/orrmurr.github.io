1be2ac4af0b299f07cd481bd3b3f22cb528b45c7
DESKTOP-OKV31AN\Go Teagyu <y2k-112@daum.net>
2018-05-12 20:49:31 +0900

    no message

--- scripts/getFilesNameFromFolder.js
+++ scripts/getFilesNameFromFolder.js
@@ -1,29 +1,10 @@
-getFileNameFromFolder = ['001.jpeg',
-'002.jpeg',
-'003.jpeg',
-'004.jpeg',
-'005.jpeg',
-'006.jpeg',
-'007.jpeg',
-'008.jpeg',
-'009.jpeg',
-'010.jpeg',
-'011.jpeg',
-'012.jpeg',
-'013.jpeg',
-'014.jpeg',
-'015.jpeg',
-'016.jpeg',
-'017.jpeg',
-'018.jpeg',
-'019.jpeg',
-'020.jpeg',
-'021.jpeg',
-'022.jpeg',
-'023.jpeg',
-'024.jpeg',
-'025.jpeg',
-'026.jpeg',
-'027.jpeg',
-'028.jpeg',
-'test20180221.png']
+function getFileNameFromFolder(){
+  $.ajax({
+      url: "../images",
+      dataType: "text",
+      success: function (getFilesName) {
+        getFileNameFromFolder = getFilesName.split("\n");
+       }
+    });
+};
+getFileNameFromFolder();
