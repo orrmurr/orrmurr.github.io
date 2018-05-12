9a850bc1afa9663c61ccc25dfd3f02dbd929f1ce
DESKTOP-OKV31AN\Go Teagyu <y2k-112@daum.net>
2018-05-12 18:19:57 +0900

    no message

--- scripts/getFilesNameFromFolder.js
+++ scripts/getFilesNameFromFolder.js
@@ -1,29 +1,12 @@
 function getFileNameFromFolder(){
-
-
-
   $.ajax({
-    url : "https://ttegululu.github.io/scripts/getFilesNameFromFolder.js",
-    dataType : "jsonp",
-    jsonp : "callback",
-    success : function(getFilesName){
-      getFileNameFromFolder = getFilesName.split("\n");
-      getFileNameFromFolder.pop();
-    }
-  });
-
-
-
-  // $.ajax({
-  //     url: "../images",
-  //     dataType: "text",
-  //     success: function (getFilesName) {
-  //       getFileNameFromFolder = getFilesName.split("\n");
-  //       getFileNameFromFolder.pop();
-  //      }
-  //   });
-
-
-
+      url: "../images",
+      dataType: "text",
+      success: function (getFilesName) {
+        getFileNameFromFolder = getFilesName.split("\n");
+        getFileNameFromFolder.pop();
+        console.log(getFilesName);
+       }
+    });
 };
 getFileNameFromFolder();
