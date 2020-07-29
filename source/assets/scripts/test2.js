1d587fccb61b4bd4a0287e4ec095c397dfbf7b28
orrmurr <36625474+orrmurr@users.noreply.github.com>
2020-07-29 17:27:24 +0900

    Update test2.js

--- source/assets/scripts/test2.js
+++ source/assets/scripts/test2.js
@@ -1,4 +1,6 @@
-﻿console.log("test success")
+import programList from "@/assets/jsons/locale/programList"
+console.log("test success")
+console.log(programList)
 
 function test() {
 	console.log("test function success")
